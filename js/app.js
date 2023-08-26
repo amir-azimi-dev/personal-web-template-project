19a199fce4b60a8bb136b3a8f6596fe563bd2b17
Amir Azimi <azymy3354@gmail.com>
2023-08-26 16:14:25 +0330

    add: header

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const navBtn = document.querySelector(".nav__hamburger-btn");
+const menu = document.querySelector(".menu");
+const cover = document.querySelector(".cover");
+const nav = document.querySelector(".nav");
+
+const toggle = () => {
+    navBtn.classList.toggle("nav__hamburger-btn--open");
+    menu.classList.toggle("menu--visible");
+    cover.classList.toggle("cover--active");
+};
+
+navBtn.addEventListener("click", toggle);
+cover.addEventListener("click", toggle);
+
+window.onscroll = () => {
+    if (window.scrollY) {
+        return nav.classList.add("nav--minimized");
+    };
+
+    nav.classList.remove("nav--minimized");
+};
